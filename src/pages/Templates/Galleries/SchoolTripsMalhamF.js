93c8ccdf5ca4dc4b6a94f92da89ff1052610cb50
mastercreator001 <melon_man2000@hotmail.com>
2023-07-18 21:51:08 +0100

    18/07/23/01

--- src/pages/Templates/Galleries/SchoolTripsMalhamF.js
+++ src/pages/Templates/Galleries/SchoolTripsMalhamF.js
@@ -79,7 +79,9 @@ function SchoolTripsMalhamF() {
               lgimage={MissFMalhamLarge}
               name="Old school Images of the school staff and students "
               position={{ color: "info", label: "Miss Forer Malham Trip" }}
-              description="Images."
+              description="As far as the saying goes (a ship sailed without her captain) the fact Miss Forer didn't go to Malham to my knowledge. Please do feel free to correct me but this section is for Miss Forer's Malham trip could be misleading, thet were her slides which she has gifted to the group so maybe she used a camera person and took a risk on what they returned who knows. <br/> <br/>
+              
+              The photo contains some of the students who attended the Malham trip in the late 1970's early 1980's we could be visiting Harewood House on the Malham Trip with Mr Monk or Northcourt House in Shorewell??. "
             />
           </MKBox>
         </Grid>
@@ -90,7 +92,7 @@ function SchoolTripsMalhamF() {
               lgimage={MalhamForer1lg}
               name="Old school Images of the school staff and students "
               position={{ color: "info", label: "Miss Forer Malham Trip" }}
-              description="Images."
+              description="We believe this to be one of the many Limestone caves visited on the trip to Malham programme 1989 possibly Monday 22nd we had a tour to Ingleborough Cave so I'm going to say this is probably the location as I cannot find any other references to caves on the Malham Programme. "
             />
           </MKBox>
         </Grid>
@@ -101,7 +103,7 @@ function SchoolTripsMalhamF() {
               lgimage={MalhamForer2lg}
               name="Old school Images of the school staff and students "
               position={{ color: "info", label: "Miss Forer Malham Trip" }}
-              description="Images."
+              description="A shot of one of the many rivers feeding the Tarn although I can't be 100% certain it still seems to be one of many nature pictures included in the pacakage. Can you see anyone in the picutre ?. "
             />
           </MKBox>
         </Grid>
@@ -112,7 +114,7 @@ function SchoolTripsMalhamF() {
               lgimage={MalhamForer3lg}
               name="Old school Images of the school staff and students "
               position={{ color: "info", label: "Miss Forer Malham Trip" }}
-              description="Images."
+              description="We have here the Famous York Minster Catherdral and also a personal tour although this was listed as optional on the Malham sheets, followed by shopping in york. Those were the days, do you remember your shopping trip?."
             />
           </MKBox>
         </Grid>
@@ -123,7 +125,7 @@ function SchoolTripsMalhamF() {
               lgimage={MalhamForer4lg}
               name="Old school Images of the school staff and students "
               position={{ color: "info", label: "Miss Forer Malham Trip" }}
-              description="Images."
+              description="Here we may have an inside view of Skipton Castle? does anyone remmeber if we were allowed to take photos. Or perhaps if you can remember this photo or the student featured it would be helpful."
             />
           </MKBox>
         </Grid>
@@ -134,7 +136,7 @@ function SchoolTripsMalhamF() {
               lgimage={MalhamForer5lg}
               name="Old school Images of the school staff and students "
               position={{ color: "info", label: "Miss Forer Malham Trip" }}
-              description="Images."
+              description="I would like to say there was a lake outside or to the side of Bolton Abbey, does anyone remember this as I may be incorrect. Oh and the ducks and their babies swimming along as well."
             />
           </MKBox>
         </Grid>
@@ -145,7 +147,7 @@ function SchoolTripsMalhamF() {
               lgimage={MalhamForer6lg}
               name="Old school Images of the school staff and students "
               position={{ color: "info", label: "Miss Forer Malham Trip" }}
-              description="Images."
+              description="Ducks and more ducks probably looking for food at Bolton Abbey, Does anyone remember paddling down there?. "
             />
           </MKBox>
         </Grid>
@@ -156,7 +158,7 @@ function SchoolTripsMalhamF() {
               lgimage={MalhamForer7lg}
               name="Old school Images of the school staff and students "
               position={{ color: "info", label: "Miss Forer Malham Trip" }}
-              description="Images."
+              description="So many possibilities that this could be but Im going with an orienterring adventure where we started out at Airhead Springs and had to map read and compass read our way back to Newfield Hall. although it could also be enroute to Gordale Scar perhaps too?."
             />
           </MKBox>
         </Grid>
@@ -167,14 +169,14 @@ function SchoolTripsMalhamF() {
               lgimage={MalhamForer8lg}
               name="Old school Images of the school staff and students "
               position={{ color: "info", label: "Miss Forer Malham Trip" }}
-              description="Images."
+              description="Yet again more ducks, I'm thinking the person loved animals who originally photographed these."
             />
           </MKBox>
         </Grid>
         <Grid container mt="30px">
           <Grid item xs={12} md={9} sx={{ mb: 6 }} mt={10}>
             <MKTypography variant="h3" color="white">
-              School Maps SVG recreation of the schools internal layout.
+              Ah and now onto the famous waterfall
             </MKTypography>
             <MKTypography variant="body2" color="white" opacity={0.8}>
               Close up of the buildings only
@@ -188,7 +190,7 @@ function SchoolTripsMalhamF() {
               lgimage={MalhamForer9lg}
               name="A close up of the map above recreated as an svg file"
               position={{ color: "info", label: "Miss Forer Malham Trip" }}
-              description="
+              description="Unknown water fall yet to be named but will come to me at some point I'm sure.
                "
             />
           </MKBox>
@@ -200,7 +202,9 @@ function SchoolTripsMalhamF() {
               lgimage={MalhamForer10lg}
               name="A close up of the map above recreated as an svg file"
               position={{ color: "info", label: "Miss Forer Malham Trip" }}
-              description="
+              description="An internal shot showing how big some of the formations had become overtime, dwarfing the young students. This should be Ingleborough Cave in Yorkshire as we all had a guided tour of the caves.<br/> <br/> 
+
+              Any ideas on the identity of the students within the cave, I wonder if they made it home?
                "
             />
           </MKBox>
